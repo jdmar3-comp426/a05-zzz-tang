5558620b62084a35e687953f550c44c452d6c5d4
zzz-tang <zziyun@email.unc.edu>
2021-11-30 18:55:29 -0500

    Revised functions for sending form data.

--- forms.js
+++ forms.js
@@ -1,11 +1,14 @@
 window.addEventListener( "load", function() {
 
+  // Access the HTML form element
+  const form = document.getElementById( "signup" );
+
   function sendData() {
 
     const sendRequest = new XMLHttpRequest();
 
     // Bind FormData object and form element
-    const signupInfo = new FormData( form );
+    const signupInfo = new URLSearchParams( new FormData( form ) );
 
     // Message for successful data submission
     sendRequest.addEventListener( "load", function( event ) {
@@ -18,20 +21,17 @@ window.addEventListener( "load", function() {
     } );
 
     // Set up request
-    sendRequest.open( "POST", "https://localhost:5000/app/new" );
+    sendRequest.open( "POST", "http://localhost:5000/app/new" );
 
     // Send data
     sendRequest.send( signupInfo );
 
   }
 
-  // Access the HTML form element
-  const form = this.document.getElementById( "signup" );
-
   // Take over submit event of form element
   form.addEventListener( "submit", function( event ) {
-    event.preventDefault;
-    sendData;
+    event.preventDefault();
+    sendData();
   } );
 
 } );
